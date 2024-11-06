7bb501866f11eeb8270fac865bbd5937275f9b53
Michael Zhou <mhzhou@andrew.cmu.edu>
2024-11-06 17:51:08 -0500

    removed prints and unused import

--- app/routes/keys/keys.py
+++ app/routes/keys/keys.py
@@ -1,21 +1,17 @@
 import configparser
 from fastapi import APIRouter, HTTPException
-import os
 from pathlib import Path
 
 router = APIRouter()
 configparser = configparser.ConfigParser()
 
 keys_dir = str(Path(__file__).parent) + "/"
-print(keys_dir)
 
 @router.get("/keys/")
 def read_keys(team_id: str):
     configparser.read(
         keys_dir + "keys.ini"
     )
-
-    print(configparser.sections())
 
     if team_id in configparser.sections():
         return configparser[team_id]
