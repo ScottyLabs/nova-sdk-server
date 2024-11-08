298b6ab10124823fed6df12719918bcbac6541a9
Michael Zhou <mhzhou@andrew.cmu.edu>
2024-11-08 11:41:22 -0500

    basic streaming integration of OpenAI Chat Completion

--- app/routes/messages/messages.py
+++ app/routes/messages/messages.py
@@ -1,7 +1,14 @@
 from enum import Enum
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from typing import List, Optional
+
+import sys
+sys.path.append("..")
+
+from ..keys import read_keys_for_team
+from utils import OpenAIModel, openai_chat_completion_streaming
 
 router = APIRouter()
 
@@ -16,26 +23,55 @@ class MessageArgs(BaseModel):
     images: Optional[List[str]] = None
     output_modality: Modality
 
-@router.post("/message/")
-def handle_message(args: MessageArgs):
+
+@router.get("/message/")
+async def handle_message(args: MessageArgs):
+    keys = read_keys_for_team(team_id=args.team_id)
+
+    if keys is None:
+        raise HTTPException(status_code=404, detail="Team ID not found")
+
     if args.images is not None:
         if args.output_modality == Modality.image:
             # Use Dall-E API
+            print("Dall-E API not implemented yet")
             pass
             # if the input contains audio, transcribe with Whisper
 
         if args.output_modality == Modality.text:
             # Use GPT4o API
+            openai_chat_completion_streaming(
+                openai_api_key=keys["OPENAI_API_KEY"],
+                model=OpenAIModel.gpt4o,
+                text=args.text,
+                images=args.images
+            )
             pass
 
         if args.output_modality == Modality.audio:
             # Use GPT4o API, and then text-to-speech the result with preferred provider
+            print("Text-to-speech API not implemented yet")
             pass
 
     elif args.output_modality == Modality.audio:
         # Use Hume API
+        print("Hume API not implemented yet")
         pass
 
     elif args.output_modality == Modality.text:
-        # Might be able to use Hume as well?
-        pass
+        completion = openai_chat_completion_streaming(
+            openai_api_key=keys["OPENAI_API_KEY"],
+            model=OpenAIModel.gpt4o,
+            text=args.text,
+            images=args.images
+        )
+
+        def completion_stream(api_response_stream):
+            for chunk in api_response_stream:
+                if chunk.choices[0].delta.content is not None:
+                    yield chunk.choices[0].delta.content
+
+        return StreamingResponse(
+            content=completion_stream(completion),
+            media_type="text/event-stream"
+        )
---
+++ app/utils/__init__.py
@@ -0,0 +1,3 @@
+from .api_calls import (
+    OpenAIModel,
+    openai_chat_completion_streaming)
