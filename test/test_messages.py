aed4fb99aae9579be93c295b805a79021d3ab695
Michael Zhou <mhzhou@andrew.cmu.edu>
2024-11-08 11:41:33 -0500

    testing streaming

---
+++ test/test_messages.py
@@ -0,0 +1,26 @@
+import httpx
+import time
+
+LOCALHOST_URL = "http://localhost:8000"
+
+def test_streaming_chat_completion():
+    stream_started = False
+    with httpx.stream(
+        "GET",
+        f"{LOCALHOST_URL}/message/",
+        json={
+            "team_id": "dev",
+            "text": "please write me an advertisement for Nova, ScottyLabs' newest event, which gives participants free access to GPT4o!",
+            "output_modality": "text"
+        }
+    ) as response:
+        for chunk in response.iter_raw():
+            if not stream_started:
+                start_time = time.time()
+                stream_started = True
+            # print(chunk)
+            continue
+
+    # We expect the stream to take at least 1 second to complete
+    assert time.time() - start_time > 1.0
+
